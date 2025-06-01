d704238de44e77cbb889571c62b686c3ecb3507f
Nenad Manev <nenad.manev@gmail.com>
2025-06-01 21:10:12 +0200

    refactor terraform.

--- video-transcriber-azure/VideoTransciber.Web/terraform-azure-setup/main.tf
+++ video-transcriber-azure/VideoTransciber.Web/terraform-azure-setup/main.tf
@@ -1,20 +1,29 @@
 provider "azurerm" {
-	features {}
+  features {}
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "transcriber-rg"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+variable "storage_container_name" {
+  description = "Name of the storage container"
+  type        = string
+  default     = "transcriber-audio"
 }
 
 resource "azurerm_resource_group" "rg" {
-	name     = "transcriber-rg"
-	location = "East US"
+  name     = var.resource_group_name
+  location = var.location
 }
-
-resource "azurerm_storage_account" "storage" {
-	name                     = "transcriberstorage"
-	resource_group_name      = azurerm_resource_group.rg.name
-	location                 = azurerm_resource_group.rg.location
-	account_tier             = "Standard"
-	account_replication_type = "LRS"
-	kind                     = "StorageV2"
-}"
 
 resource "random_string" "suffix" {
   length  = 5
@@ -23,8 +32,5 @@ resource "random_string" "suffix" {
   special = false
 }
 
-resource "azurerm_storage_container" "container" {
-  name                  = "transcriber-audio"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
-}
+resource "azurerm_storage_account" "storage" {
+  name                    
