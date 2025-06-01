a1cfdd70e2353733e1e3cf39732187b6a1186541
Nenad Manev <nenad.manev@gmail.com>
2025-06-01 23:23:45 +0200

    apply terraform

--- video-transcriber-azure/VideoTransciber.Web/terraform-azure-setup/main.tf
+++ video-transcriber-azure/VideoTransciber.Web/terraform-azure-setup/main.tf
@@ -1,30 +1,48 @@
 provider "azurerm" {
-	features {}
+  features {}
+  subscription_id = "e89ec99d-4196-49df-aa73-e842e9574661"
 }
 
-resource "azurerm_resource_group" "rg" {
-	name     = "transcriber-rg"
-	location = "East US"
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "transcriber-rg"
 }
 
-resource "azurerm_storage_account" "storage" {
-	name                     = "transcriberstorage"
-	resource_group_name      = azurerm_resource_group.rg.name
-	location                 = azurerm_resource_group.rg.location
-	account_tier             = "Standard"
-	account_replication_type = "LRS"
-	kind                     = "StorageV2"
-}"
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+variable "storage_container_name" {
+  description = "Name of the storage container"
+  type        = string
+  default     = "transcriber-audio"
+}
 
 resource "random_string" "suffix" {
   length  = 5
   upper   = false
-  number  = true
+  numeric = true
   special = false
 }
 
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "transcriber${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
 resource "azurerm_storage_container" "container" {
-  name                  = "transcriber-audio"
+  name                  = var.storage_container_name
   storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
--- video-transcriber-azure/VideoTransciber.Web/terraform-azure-setup/outputs.tf
+++ video-transcriber-azure/VideoTransciber.Web/terraform-azure-setup/outputs.tf
@@ -10,3 +10,11 @@ output "connection_string" {
   value     = azurerm_storage_account.storage.primary_connection_string
   sensitive = true
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "location" {
+  value = azurerm_resource_group.rg.location
+}
