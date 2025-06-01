63807ec55c36bc7813d2a1982a17e9693583f22f
Nenad Manev <nenad.manev@gmail.com>
2025-06-01 18:44:45 +0200

    create mvc project

---
+++ video-transcriber-azure/VideoTransciber.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VideoTransciber.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
